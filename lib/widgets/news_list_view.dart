c2d98e7f4b77e457e601c8b397cbbead65cc14b1
osamaaboharoon <osamaaboharoon@gmail.com>
2025-05-24 14:03:18 +0300

    loding

--- lib/widgets/news_list_view.dart
+++ lib/widgets/news_list_view.dart
@@ -1,51 +1,45 @@
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp/models/article_model.dart';
 import 'package:newsapp/service/news_service.dart';
+import 'package:newsapp/widgets/loading.dart';
 import 'package:newsapp/widgets/news_tile.dart';
 
 class NewsListView extends StatefulWidget {
-  const NewsListView({
-    super.key,
-  });
+  const NewsListView({super.key});
 
   @override
   State<NewsListView> createState() => _NewsListViewState();
 }
 
 class _NewsListViewState extends State<NewsListView> {
-  List<ArticleModel> articles =[];
+  List<ArticleModel> articles = [];
+  bool isLoding =true;
   @override
-  void initState()  {
+  void initState() {
     super.initState();
-   getGenaralNews();
-
+    getGenaralNews();
   }
 
   Future<void> getGenaralNews() async {
     articles = await NewsService(Dio()).gitGenarlNews();
-    setState(() {
-      
-    });
+    isLoding =false;
+    setState(() {});
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      
-      delegate: SliverChildBuilderDelegate(
-        childCount: articles.length,
-      (Context,Index){
-              return 
-               Padding(
+   
+    return !isLoding ? SliverFillRemaining(child: Loadings()): SliverList(
+      delegate: SliverChildBuilderDelegate(childCount: articles.length, (
+        Context,
+        Index,
+      ) {
+        return Padding(
           padding: const EdgeInsets.only(bottom: 22),
           child: NewsTile(articleModel: articles[Index]),
         );
-            },
-           
-            )
-    );  }
+      }),
+    );
+  }
 }
